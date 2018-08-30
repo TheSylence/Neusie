17f48ac9c7eebe7cc4e8496003f37fa34d9d6e2c
Matthias Specht <sylence@btbsoft.org>
2018-08-30 16:23:50 +0200

    Added CollisionMap to check for rectangle collision (unoptimized)

---
+++ Neusie.Tests/Generation/Image/CollisionMapTests.cs
@@ -0,0 +1,206 @@
+﻿using System.Drawing;
+using System.Linq;
+using Neusie.Generation.Image;
+using NUnit.Framework;
+
+namespace Neusie.Tests.Generation.Image
+{
+	[TestFixture]
+	internal class CollisionMapTests
+	{
+		[TestFixture]
+		internal class Check
+		{
+			[Test]
+			public void ShouldBePossibleWhenMapIsEmpty()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new RectangleF( 1, 1, 3, 3 ) );
+
+				// Assert
+				Assert.IsTrue( actual );
+			}
+
+			[Test]
+			public void ShouldBePossibleWithNonOverlappingRectangle()
+			{
+				// Arrange
+				var sut = new CollisionMap( 10, 10 );
+				sut.Insert( new RectangleF( 1, 1, 1, 1 ) );
+
+				// Act
+				var actual = sut.Check( new RectangleF( 4, 4, 1, 1 ) );
+
+				// Assert
+				Assert.IsTrue( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForBottomBorder()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( 1, 4, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForLeftBorder()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( -1, 1, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForOutOfBottomBound()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( 1, 6, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForOutOfLeftBound()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( -5, 1, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForOutOfRightBound()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( 6, 1, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForOutOfTopBound()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( 1, -5, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForRightBorder()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( 4, 1, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldCheckForTopBorder()
+			{
+				// Arrange
+				var sut = new CollisionMap( 5, 5 );
+
+				// Act
+				var actual = sut.Check( new Rectangle( 1, -1, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldNotBePossibleWhenCompletelyInsideExistingRect()
+			{
+				// Arrange
+				var sut = new CollisionMap( 10, 10 );
+				sut.Insert( new RectangleF( 1, 1, 7, 7 ) );
+
+				// Act
+				var actual = sut.Check( new RectangleF( 3, 3, 1, 1 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+
+			[Test]
+			public void ShouldNotBePossibleWhenOverlappingWithExisting()
+			{
+				// Arrange
+				var sut = new CollisionMap( 10, 10 );
+				sut.Insert( new RectangleF( 1, 1, 3, 3 ) );
+
+				// Act
+				var actual = sut.Check( new RectangleF( 2, 2, 3, 3 ) );
+
+				// Assert
+				Assert.IsFalse( actual );
+			}
+		}
+
+		[TestFixture]
+		internal class Rectangles
+		{
+			[Test]
+			public void ShouldBeEmptyWhenNothingWasAdded()
+			{
+				// Arrange
+				var sut = new CollisionMap( 1, 1 );
+
+				// Act
+				var actual = sut.Rectangles.ToList();
+
+				// Assert
+				CollectionAssert.IsEmpty( actual );
+			}
+
+			[Test]
+			public void ShouldContainAddedElements()
+			{
+				// Arrange
+				var sut = new CollisionMap( 1, 1 );
+				var expected = new RectangleF( 1, 1, 1, 1 );
+				sut.Insert( expected );
+
+				// Act
+				var actual = sut.Rectangles.ToList();
+
+				// Assert
+				CollectionAssert.Contains( actual, expected );
+			}
+		}
+	}
+}
---
+++ Neusie/Generation/Image/CollisionMap.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Neusie.Generation.Image
+{
+	internal class CollisionMap
+	{
+		public CollisionMap( int width, int height )
+		{
+			Width = width;
+			Height = height;
+
+			_Rectangles.Add( new RectangleF( 0, 0, Width, 1 ) );
+			_Rectangles.Add( new RectangleF( 0, 0, 1, Height ) );
+			_Rectangles.Add( new RectangleF( Width - 1, 0, 1, Height ) );
+			_Rectangles.Add( new RectangleF( 0, Height - 1, Width, 1 ) );
+		}
+
+		public bool Check( RectangleF rect )
+		{
+			return CheckBounds( rect ) && CheckCollision( rect );
+		}
+
+		public void Insert( RectangleF rect )
+		{
+			_Rectangles.Add( rect );
+		}
+
+		private bool CheckBounds( RectangleF rect )
+		{
+			if( rect.Right >= Width )
+			{
+				return false;
+			}
+
+			if( rect.Left <= 0 )
+			{
+				return false;
+			}
+
+			if( rect.Bottom <= 0 )
+			{
+				return false;
+			}
+
+			if( rect.Top >= Height )
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		private bool CheckCollision( RectangleF rect )
+		{
+			foreach( var exitingRectangle in _Rectangles )
+			{
+				if(exitingRectangle.IntersectsWith( rect ) )
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public IEnumerable<RectangleF> Rectangles => _Rectangles.Skip( 4 );
+
+		private readonly List<RectangleF> _Rectangles = new List<RectangleF>();
+
+		private readonly int Height;
+		private readonly int Width;
+	}
+}
